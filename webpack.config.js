bef25810e3729dace5034e90ed61db29946c4152
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-07-30 00:51:01 -0400

    webpack copy some stuf

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,9 +8,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: "public/img", to: "img" },
+        { from: "public/style", to: "style" },
+      ],
+    }),
   ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
