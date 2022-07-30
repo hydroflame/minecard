559389bd8ea4d6c17c473bd93cd3c70b3a21a7fe
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-07-30 01:03:34 -0400

    in ts

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
